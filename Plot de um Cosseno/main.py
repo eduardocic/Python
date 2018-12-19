2145a6352c7206d00bdc14a2b2e26a8a1406f80d
Eduardo <eduardo.ime12@gmail.com>
2018-12-19 16:22:58 -0200

    Iniciando o trabalho com arquivos e funções por meio do Python

---
+++ Plot de um Cosseno/main.py
@@ -0,0 +1,23 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as o
+
+
+x, COS, SEN = [], [], []
+
+
+for i in range(0, 360, 1):
+    x.append(i)
+    COS.append(o.cos((o.pi)*i/180))
+    SEN.append(o.sin((o.pi)*i/180))
+
+
+""" Plot do resultado do sistema """
+plt.plot(x, COS, label = 'cosseno') 
+plt.hold
+plt.plot(x, SEN, '-r', label = 'seno')
+plt.xlabel("Graus")
+plt.ylabel("Resultado")
+plt.title("Seno e Cosseno")
+plt.grid()
+plt.legend()
