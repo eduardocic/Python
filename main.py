75340a93067ca109b74cba8714de41ea1c3efcc2
Eduardo <eduardo.ime12@gmail.com>
2019-01-10 17:13:35 -0200

    Utilização das variáveis para FFT

---
+++ main.py
@@ -0,0 +1,46 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as o
+import cmath
+
+# Número 'pi'.
+pi  = cmath.pi
+
+# Número complexo.
+num = complex(0, pi)
+
+# Exponencial do número.
+b   = cmath.exp(num)
+
+
+
+
+#x, COS, SEN = [], [], []
+#
+#
+#for i in range(0, 360, 1):
+#    x.append(i)
+#    COS.append(o.cos((o.pi)*i/180))
+#    SEN.append(o.sin((o.pi)*i/180))
+#
+#
+#""" Plot do resultado do sistema """
+#plt.plot(x, COS, label = 'cosseno') 
+#plt.hold
+#plt.plot(x, SEN, '-r', label = 'seno')
+#plt.xlabel("Graus")
+#plt.ylabel("Resultado")
+#plt.title("Seno e Cosseno")
+#plt.grid()
+#plt.legend()
+#
+#""" Plotando em outra figura """
+#plt.figure()
+#plt.plot(x, COS, label = 'cosseno') 
+#plt.xlabel("Graus")
+#plt.ylabel("Resultado")
+#plt.grid()
+
+
+
+
